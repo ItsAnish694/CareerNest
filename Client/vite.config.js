d7c41cc021e6c0bdb06c229a138369537642b2c7
Anish Dhakal <anishdhakal210@gmail.com>
2025-07-09 23:29:07 +0545

    Base Url

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      // "/api/v1": "http://127.0.0.1:8000",
       "/api/v1": "https://careernest-backend-plwn.onrender.com",
+      // "/api/v1": "http://127.0.0.1:8000",
     },
   },
 });
